b67fa8d5d33959b962d97be04d73891b4ea8c684
Dario Arkus <dario@arkus.ca>
2020-07-06 18:21:49 -0700

    helper functions

--- TimeToDie.lua
+++ TimeToDie.lua
@@ -45,6 +45,34 @@ function SecondsToClock(seconds)
     return hours .. ":" .. mins .. ":" .. secs
 end
 
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
+
 local _G = getfenv(0);
 local abs = _G.abs;
 local min = _G.min;
